1bbf3099dbb72168276905812f8d74a03ca73b92
BALAJI M <balajim2063@gmail.com>
2026-01-06 22:54:49 +0530

    Update 0088-merge-sorted-array.java

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -16,27 +16,7 @@ class Solution
             System.out.print(Arrays.toString(nums1));
         }
         
-  /*      if( n && m >1)
-        {
-            int i =0;  // for arr 1 
-            int j =0;  // arr 2 
 
-            while(j<n)
-            {
-              if(nums1[i]==nums2[j] || nums1[i]>nums2[j])
-              {
-                
-              }
-              else if (nums1[i]<nums2[j])
-              {
-                  i++;
-              }
-              
-              
-            }
-        }
-
-        */
 
 if( n>0 && m >0)
 {
