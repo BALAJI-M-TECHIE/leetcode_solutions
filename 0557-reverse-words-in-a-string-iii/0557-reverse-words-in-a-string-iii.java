cfe964e5376589046bb39ab6cfbecb1a3bbf91a9
BALAJI M <balajim2063@gmail.com>
2026-01-08 21:01:09 +0530

    Time: 19 ms (14.22%), Space: 46.6 MB (71.57%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -5,48 +5,24 @@ class Solution {
         int last_index = 0;
         int i =0;
 
-        while(i<sb.length())
+        while(i<=sb.length())
         {
-            if(sb.charAt(i)==' ')
-            {
-                int z_start = last_index;
-                int z_last = i-1;
+            if(i==sb.length()||sb.charAt(i)==' ' )
+            {            
+                 int  z_last = i-1;              
+                int z_start = last_index;               
                 while(z_start<z_last)
                 {
                   char temp = sb.charAt(z_start);
                   sb.setCharAt(z_start,sb.charAt(z_last));
                   sb.setCharAt(z_last,temp);
-
                   z_start++;
                   z_last--;
-
                 }
                 last_index = i+1;
             }
-
-            if(i==sb.length()-1)
-            {
-                 
-                int z_start = last_index;
-                int z_last =i;
-                while(z_start<z_last)
-                {
-                  char temp = sb.charAt(z_start);
-                  sb.setCharAt(z_start,sb.charAt(z_last));
-                  sb.setCharAt(z_last,temp);
-
-                  z_start++;
-                  z_last--;
-
-                }
-            
-            }
             i++;
-            
-
         }
- 
- return sb.toString();
-        
+ return sb.toString();       
     }
 }
