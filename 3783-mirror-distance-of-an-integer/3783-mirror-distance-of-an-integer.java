93c020633cf38596ba24375f53fb817a985dc209
BALAJI M <balajim2063@gmail.com>
2026-05-22 15:32:39 +0530

    Time: 1 ms (99.88%), Space: 42.2 MB (90.32%) - LeetHub

--- 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.java
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.java
@@ -1,6 +1,27 @@
 class Solution {
     public int mirrorDistance(int n) {
 
+             if(n<10)
+                return 0;
+
+              else
+              {
+                int revNum=0;
+                int num = n;
+
+                while(num>9)
+                {
+                    revNum= revNum*10+(num%10);
+                    num = num/10;
+                }
+                revNum=revNum*10+ num;
+
+                return Math.abs(n - revNum);
+              }
+
+
+
+/* /// USING STRING
         if(n<10)
         return 0;
 
@@ -20,5 +41,8 @@ class Solution {
 
 
         }
+
+
+        */
     }
 }
