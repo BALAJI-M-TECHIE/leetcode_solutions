c3595227e300dbe453412db1c0304bb62d62334a
BALAJI M <balajim2063@gmail.com>
2026-06-16 23:07:39 +0530

    Time: 3 ms (77.46%), Space: 46.7 MB (99.31%) - LeetHub

---
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.java
@@ -0,0 +1,42 @@
+class Solution 
+{
+    public int[][] largestLocal(int[][] grid) 
+    {
+        int arr_size = grid.length;
+        int[][] new_arr = new int[arr_size -2][arr_size-2];
+       int[] num = new int[(arr_size - 2 )*(arr_size - 2 )] ;
+
+       int e = 0;
+        for(int i =0;i<=arr_size - 3 ;i++)
+        {
+            for(int j =0 ; j<= arr_size - 3 ;j++)
+            {
+                int max = 0;
+                for(int r = i ; r <i+3;r++)
+                {
+                    for(int c = j ; c<j+3;c++)
+                    {
+                       if(max<grid[r][c])
+                       {
+                        max = grid[r][c];
+                       }
+                    }
+                }
+                 num[e] = max;
+                        e++;
+            }
+        }
+
+        int z =0;
+        for(int i =0;i<arr_size-2;i++)
+        {
+            for(int j =0;j<arr_size-2;j++)
+            {
+               new_arr[i][j] = num[z]; 
+               z++;
+            }
+        }
+
+        return new_arr;
+    }
+}
