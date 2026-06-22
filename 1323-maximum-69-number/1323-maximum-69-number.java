006aaee5e650174351d80b4f1e3a7bdf4c65007e
BALAJI M <balajim2063@gmail.com>
2026-06-23 03:14:53 +0530

    Time: 1 ms (79.51%), Space: 42.5 MB (12.49%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximum69Number (int num) {
+        
+        String n =String.valueOf(num);
+        char[] arr = n.toCharArray();
+        for(int i =0 ;i<arr.length;i++)
+        {
+            if(arr[i]=='6')
+            {
+                arr[i]='9';
+                break;
+            }
+        }
+
+        return Integer.parseInt(String.valueOf(arr));
+    }
+}
