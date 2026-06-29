1635cb188710d2c3c9766c5486e916c2d2296128
BALAJI M <balajim2063@gmail.com>
2026-06-29 18:56:10 +0530

    Time: 2 ms (29.27%), Space: 45.5 MB (48.82%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,101 @@
+class Solution {
+    public int compress(char[] chars) {
+
+        /*
+
+        int[] arr = new int[123];
+
+        for(char i : chars)
+        {
+            arr[i]++;
+        }        
+
+        int count=0;
+        int ind = 0;
+
+        StringBuffer sb = new StringBuffer();
+        for(int i =0;i<123;i++)
+        {
+            boolean result = arr[i]>1;
+            boolean result_2 = arr[i]==1;
+
+            if(result)
+            {
+                sb.append((char)i);
+                sb.append(arr[i]);
+                if(arr[i]>9)
+                {
+                    count++;
+                }
+                count+=2;
+            }
+            else
+            {
+                if(result_2)
+                {
+                    sb.append((char)i);
+                    count++;
+                }
+            }
+        }
+
+        // modifying the chars 
+
+        for(int i =0;i<sb.length();i++)
+        {
+            chars[i]=sb.charAt(i);
+        }
+
+        return count;
+
+*/
+
+        StringBuffer sb = new StringBuffer();
+
+        int i =0; int j=0;
+
+        while( j<chars.length)
+        {
+            if(chars[i]==chars[j])
+            {
+                if(j+1 ==chars.length)
+                {
+                     sb.append(chars[i]);
+                    if(((j-i)+1)>1)
+                    {
+                        sb.append((j-i)+1); 
+                    }
+                    break;
+                
+                }
+                else
+                {
+                    j++; 
+                }
+               
+            }
+            else
+            {
+                sb.append(chars[i]);
+                if((j-i)>1)
+                {
+                   sb.append(j-i); 
+                }
+                
+                i=j;
+                
+            }
+        }
+                
+
+         // modifying the chars 
+         int len = sb.length();
+
+        for(int k =0;k<len;k++)
+        {
+            chars[k]=sb.charAt(k);
+        }
+        return len;
+
+    }
+}
