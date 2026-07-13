e78714b4392cd2e0ea4e568190c6352e1f819660
BALAJI M <balajim2063@gmail.com>
2026-07-13 11:56:48 +0530

    Time: 0 ms (100%), Space: 44 MB (87.37%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -6,7 +6,7 @@ class Solution {
 
         // BRUTE APPROACH :
         
-        if(nums.length == 1)
+      /*  if(nums.length == 1)
         {
             return 0;
         }
@@ -30,6 +30,51 @@ class Solution {
            
         }
         
-        return -1;
+        return -1;     */
+
+
+        // TRYING BINARY SEARCH : 
+
+         if(nums.length == 1)
+        {
+            return 0;
+        }
+        if(nums[0]>nums[1])
+        {
+            return 0;
+        }
+        else if(nums[nums.length-1]>nums[nums.length-2])
+        {
+            return nums.length-1;
+        }
+
+        int left = 1 ;
+        int right = nums.length-2;
+       
+
+        while(left<=right)
+        {
+            int mid = left+((right - left)/2);
+
+
+            if(nums[mid]>nums[mid-1]  && nums[mid]>nums[mid+1])
+            {
+                return mid;
+            }
+            // which side gonna move to find the peak ? 
+            // move right : 
+
+            else if(nums[mid]<nums[mid+1])
+            {
+                left = mid+1;
+            }
+            else
+            {
+                right = mid-1;
+            }
+
+        }
+
+        return 0;
     }
 }
