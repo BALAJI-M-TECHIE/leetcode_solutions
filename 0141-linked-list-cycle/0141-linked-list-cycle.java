7b4ace27fc16eeabb1ecd1c8c4785dbe6d7a9b44
BALAJI M <balajim2063@gmail.com>
2026-07-25 11:38:42 +0530

    Time: 1 ms (13.65%), Space: 46.9 MB (33.42%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -13,7 +13,7 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
 
         // storing the memory address and check , if already present means loop detected : , if ll has the null means that is false : 
-
+/*
         if(head==null || head.next == null)
         {
             return false;
@@ -36,6 +36,24 @@ public class Solution {
             temp = temp.next;
         }
 
-        return false;
+        return false;   */
+
+    // The number of the nodes in the list is in the range [0, 10 pow 4].  
+
+    int len = 0;
+
+    ListNode temp = head;
+
+    while(temp!=null)
+    {
+        len++;
+        if(len>10000)
+        {
+            return true;
+        }
+        temp= temp.next;
+    }
+return false;
+
     }
 }
